b70a125c19c9cb45dd218ac1e17e7005288b81ed
Victoria Aulogia <vlaulogia@gmail.com>
2023-01-28 15:49:26 -0800

    variables and functions examples

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
 // grab DOM elements
 
 // set event listeners
+console.log('hello world!');
+console.log('42', 42);
+const dogName = 'Benny';
+console.log(`My dogs name is ${dogName}. ${dogName} is very cute. ${dogName} is 7 years old.`);
+
+
+//functions
+//allow us to reuse behavior or multiple lines of code
+
+function sayHello(name) {
+    console.log(`Hello ${name}!`);
+    console.log('It is a beautiful day today!');
+    console.log('I hope you are well!');
+}
+
+sayHello('Justin');
+sayHello('Natasha');
