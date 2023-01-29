3b010a9ebf2c20c52411cef8dd6ba4cc6025797d
Victoria Aulogia <vlaulogia@gmail.com>
2023-01-28 16:40:48 -0800

    the name tag updates based on the name input

--- app.js
+++ app.js
@@ -18,3 +18,24 @@ function sayHello(name) {
 
 sayHello('Justin');
 sayHello('Natasha');
+
+//make a variable that contains a reference to the html element
+const nameElement = document.getElementById('name');
+//change the text content of that html element
+nameElement.textContent = 'Vicky';
+
+//make a variable that references the update button
+//listen for a click on that button
+const updateBtn = document.getElementById('update');
+
+updateBtn.addEventListener('click', ()=> {
+    console.log('i am clicking on the button!!');
+    //make a variable that references the INPUT value
+    const nameInput = document.getElementById('input');
+    //make a variable that contains a reference to 
+    //the name html element
+    const nameElem = document.getElementById('name');
+    //change the text content of the name html element to the value
+    //of the input
+    nameElem.textContent = nameInput.value;
+});
